f87958f8b21ddb76a4a95af7393b22196736d76f
grandbusta <bustajay30@gmail.com>
2021-05-09 20:53:46 +0100

    user controller funtions updated

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,6 @@
 const { User } = require('../models')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const validator = require('email-validator')
 const { JWT_KEY } = process.env
 
@@ -46,11 +47,83 @@ const signUp = async (req, res, next) => {
   }
 }
 
-const login = (req, res, next) => {}
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    if (email && password) {
+      if (validator.validate(email)) {
+        let check = await User.findOne({
+          where: { email: email },
+          attributes: ['email', 'password'],
+        })
+        if (check !== null) {
+          let comparePassword = await bcrypt.compare(password, check.password)
+          if (comparePassword) {
+            res.status(200).json({
+              response: 'Auth successful',
+              token: jwt.sign(
+                {
+                  email: check.email,
+                },
+                JWT_KEY,
+              ),
+            })
+          } else {
+            res.status(401).json({
+              response: 'Auth failed',
+            })
+          }
+        } else {
+          res.status(404).json({
+            response: 'user not found',
+          })
+        }
+      } else {
+        res.status(422).json({
+          response: 'not a valid email',
+        })
+      }
+    } else {
+      res.status(422).json({
+        response: 'one or more values are missing',
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      response: 'An error occured',
+    })
+  }
+}
 
-const remove = (req, res, next) => {}
+const remove = (req, res, next) => {
+  try {
+		await User.destroy({where:{id:req.userData.id}})
+		res.status(204).json({
+			response:'user deleted sucessfully'
+		})
+	} catch (error) {
+		res.status(500).json({
+			message:'An error occured'
+		})
+	}
+}
 
-const update = (req, res, next) => {}
+const update = (req, res, next) => {
+  try {
+		await User.update(
+			{...req.body},
+			{where:{id:req.userData.id}}
+		)
+		res.status(204).json({
+			message:'update successful'
+		})
+	} catch (error) {
+		res.status(500).json({
+			message:'An error occured'
+		})
+	}
+}
 
 module.exports = {
   signUp,
